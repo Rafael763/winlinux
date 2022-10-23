4cd76bf6f02bea3cf0854b2e813683e680b483a6
Rafael <81027973+Rafael763@users.noreply.github.com>
2022-10-23 10:24:34 -0300

    começo do login do sistema operasional
    o sistema operacional devera ter segurança contra crakkers militar e fusão das funcionalidades do windows e linux

---
+++ login.lua
@@ -0,0 +1,9 @@
+print("ola crie sua senha")
+    local senha = io.read()
+print("insira a sua senha para fazer o login")
+    if senha == io.read() then
+print ("bem vindo") 
+    else 
+ print("senha errada")
+    end
+ 
