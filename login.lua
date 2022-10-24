63e0d9017e0bab5c17640b663758c47b9ad5e513
Rafael <81027973+Rafael763@users.noreply.github.com>
2022-10-24 06:55:39 -0300

    coreção de bugs do login do sistema operacional
    coreçãode bugs

--- login.lua
+++ login.lua
@@ -1,9 +1,11 @@
-print("ola crie sua senha")
+for x =1,3,1 do
+    print("ola crie sua senha")
     local senha = io.read()
-print("insira a sua senha para fazer o login")
+    print("insira a sua senha para fazer o login")
     if senha == io.read() then
-print ("bem vindo") 
+        print ("bem vindo") 
+        break
     else 
- print("senha errada")
+        print("senha errada")
     end
- 
+end
